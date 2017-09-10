8f3eb933715cf2c85b4319cede816cc019f6cfc7
Dylan-Halls <dylan.halls123@gmail.com>
2017-09-10 22:35:19 +0100

    added setsockopt to reuse port

--- Proxy/proxy.c
+++ Proxy/proxy.c
@@ -9,7 +9,6 @@
 #include <sys/types.h>
 
 /*
-
 for connection_handler handler
 
 char buffer[1024];
@@ -20,22 +19,35 @@ close(sockfd)
 
 #define port 8000
 
+void get_host(char headers[]){
+  int i;
+  for (i = 0; i < strlen(headers); i++) {
+    if (strcmp(&headers[i], "H") == 0) {
+      //printf("%s\n", "Ran");
+      printf("%c\n", headers[i]+6);
+    }
+  }
+}
+
 void sock_send(const char *text, int sockfd){
   char buffer[6000];
   strcpy(buffer, text);
   write(sockfd, buffer, strlen(buffer));
-  close(sockfd);
 }
 
 void connection_handler(int sockfd){
   int read_size;
   char readbuffer[1024];
   char headers[4000];
+  printf("%s\n", "Connection");
   while (1){
     if ((read_size = recv(sockfd, readbuffer, 2000, 0)) > 0) {
       strcat(headers, readbuffer);
     }
+    get_host(headers);
     sock_send(headers, sockfd);
+    close(sockfd);
+    return;
   }
 }
 
@@ -57,6 +69,16 @@ int main(int argc, char const *argv[]) {
 
   bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
   printf("%s\n", "Binded");
+
+  /*
+  int setsockopt(int socket, int level, int option_name,
+const void *option_value, socklen_t option_len);
+  */
+  int error;
+  int sockopt = 1;
+  int fd = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &error, sizeof(sockopt));
+  printf("%d\n", fd);
+  printf("%d\n", error);
 
   if(listen(listenfd, 10) == -1){
       printf("Failed to listen\n");
